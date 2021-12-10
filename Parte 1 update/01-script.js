2c1fc3e8da69848eff4b8c18dd88d4b326e46424
Argusto Lima <augusto.vlima2@gmail.com>
2021-12-10 18:45:24 -0300

    HTML5 e CSS3 parte 1: A primeira página da Web - Atualização 1.1

---
+++ Parte 1 update/01-script.js
@@ -0,0 +1,15 @@
+function info() {
+    var resul = window.document.getElementById('resultado')
+    resul.innerHTML = `
+    Localizado no Pinheirinho da cidade de Curitiba,
+    a barbearia Alura traz ao mercado o que há de melhor para o seu
+    cabelo e barba, Fundada em 2019, a barbearia já é destaque
+    na cidade e conquista novos clientes a cada dia. <br><br>
+    
+    Nossa missão é: "Proporcionar auto-estima e qualidade de vida aos clientes!"<br><br>
+
+    Oferecemos profissionais experientes e antenados às mudanças no mundo da moda.
+    O atendimento possui padrão de excelência e agilidade, garantindo qualidade e
+    satisfação dos nossos clientes.
+    `
+}
